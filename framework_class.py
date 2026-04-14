cc31339d6f5f96de777ac8d1c82f130f588b755b
izadora ganem <izadora.ganem@tarken.ag>
2026-04-13 22:34:13 -0300

    5. Classe TestSuite

--- framework_class.py
+++ framework_class.py
@@ -128,6 +128,50 @@ class TestCaseTest(TestCase):
         assert spy.log == "set_up test_method tear_down"
 
 
+class TestSuite:
+
+    def __init__(self):
+        self.tests = []
+
+    def add_test(self, test):
+        self.tests.append(test)
+
+    def run(self, result):
+        for test in self.tests:
+            test.run(result)
+
+
+class TestSuiteTest(TestCase):
+
+    def test_suite_size(self):
+        suite = TestSuite()
+
+        suite.add_test(TestStub('test_success'))
+        suite.add_test(TestStub('test_failure'))
+        suite.add_test(TestStub('test_error'))
+
+        assert len(suite.tests) == 3
+
+    def test_suite_success_run(self):
+        result = TestResult()
+        suite = TestSuite()
+        suite.add_test(TestStub('test_success'))
+
+        suite.run(result)
+
+        assert result.summary() == '1 run, 0 failed, 0 error'
+
+    def test_suite_multiple_run(self):
+        result = TestResult()
+        suite = TestSuite()
+        suite.add_test(TestStub('test_success'))
+        suite.add_test(TestStub('test_failure'))
+        suite.add_test(TestStub('test_error'))
+
+        suite.run(result)
+
+        assert result.summary() == '3 run, 1 failed, 1 error'
+
 class MyTest(TestCase):
 
     def test_a(self):
--- main.py
+++ main.py
@@ -2,30 +2,21 @@ import framework_class
 
 if __name__ == "__main__":
     
-        result = framework_class.TestResult()
+    result = framework_class.TestResult()
+    suite = framework_class.TestSuite()
 
-        test = framework_class.TestCaseTest('test_result_success_run')
-        test.run(result)
+    suite.add_test(framework_class.TestCaseTest('test_result_success_run'))
+    suite.add_test(framework_class.TestCaseTest('test_result_failure_run'))
+    suite.add_test(framework_class.TestCaseTest('test_result_error_run'))
+    suite.add_test(framework_class.TestCaseTest('test_result_multiple_run'))
+    suite.add_test(framework_class.TestCaseTest('test_was_set_up'))
+    suite.add_test(framework_class.TestCaseTest('test_was_run'))
+    suite.add_test(framework_class.TestCaseTest('test_was_tear_down'))
+    suite.add_test(framework_class.TestCaseTest('test_template_method'))
 
-        test = framework_class.TestCaseTest('test_result_failure_run')
-        test.run(result)
+    suite.add_test(framework_class.TestSuiteTest('test_suite_size'))
+    suite.add_test(framework_class.TestSuiteTest('test_suite_success_run'))
+    suite.add_test(framework_class.TestSuiteTest('test_suite_multiple_run'))
 
-        test = framework_class.TestCaseTest('test_result_error_run')
-        test.run(result)
-
-        test = framework_class.TestCaseTest('test_result_multiple_run')
-        test.run(result)
-
-        test = framework_class.TestCaseTest('test_was_set_up')
-        test.run(result)
-
-        test = framework_class.TestCaseTest('test_was_run')
-        test.run(result)
-
-        test = framework_class.TestCaseTest('test_was_tear_down')
-        test.run(result)
-
-        test = framework_class.TestCaseTest('test_template_method')
-        test.run(result)
-
-        print(result.summary())
+    suite.run(result)
+    print(result.summary())
