037075a3e3bf7576c0b2bf52eee792caccb9338c
izadora ganem <izadora.ganem@tarken.ag>
2026-04-13 21:27:20 -0300

    4. Testando TestCase

--- main.py
+++ main.py
@@ -2,15 +2,30 @@ import framework_class
 
 if __name__ == "__main__":
     
-    result = framework_class.TestResult()
+        result = framework_class.TestResult()
 
-    test = framework_class.MyTest('test_a')
-    test.run(result)
+        test = framework_class.TestCaseTest('test_result_success_run')
+        test.run(result)
 
-    test = framework_class.MyTest('test_b')
-    test.run(result)
+        test = framework_class.TestCaseTest('test_result_failure_run')
+        test.run(result)
 
-    test = framework_class.MyTest('test_c')
-    test.run(result)
+        test = framework_class.TestCaseTest('test_result_error_run')
+        test.run(result)
 
-    print(result.summary())
+        test = framework_class.TestCaseTest('test_result_multiple_run')
+        test.run(result)
+
+        test = framework_class.TestCaseTest('test_was_set_up')
+        test.run(result)
+
+        test = framework_class.TestCaseTest('test_was_run')
+        test.run(result)
+
+        test = framework_class.TestCaseTest('test_was_tear_down')
+        test.run(result)
+
+        test = framework_class.TestCaseTest('test_template_method')
+        test.run(result)
+
+        print(result.summary())
