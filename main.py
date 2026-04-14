4de3e19b221abda5f1576513f326a18e8ba0c291
izadora ganem <izadora.ganem@tarken.ag>
2026-04-13 22:52:50 -0300

    7. Executando Todos os Testes

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ import framework_class
 if __name__ == "__main__":
     
     loader = framework_class.TestLoader()
-    suite = loader.make_suite(framework_class.TestLoaderTest)
+    test_case_suite = loader.make_suite(framework_class.TestCaseTest)
+    test_suite_suite = loader.make_suite(framework_class.TestSuiteTest)
+    test_load_suite = loader.make_suite(framework_class.TestLoaderTest)
+
+    suite = framework_class.TestSuite()
+    suite.add_test(test_case_suite)
+    suite.add_test(test_suite_suite)
+    suite.add_test(test_load_suite)
 
     runner = framework_class.TestRunner()
     runner.run(suite)
